5ed388f0dca56648eaf8d83775c3ead70c2c2a1c
Naman kumar <namankr4582@gmail.com>
2026-05-28 13:47:07 +0530

    Refactor post generation logic in post_generator.py

--- post_generator.py
+++ post_generator.py
@@ -22,4 +22,24 @@ def get_prompt(length, language, tag):
 
     prompt = f'''Generate a LinkedIn post using the below information. No preamble.
 
-1) Topic
+1) Topic: {tag}
+2) Length: {length_str}
+3) Language: {language}
+If Language is Hinglish then it means it is a mix of Hindi and English. 
+The script for the generated post should always be written in standard English alphabet script characters.
+'''
+    try:
+        few_shot = FewShotPosts()
+        examples = few_shot.get_filtered_posts(length, language, tag)
+
+        if len(examples) > 0:
+            prompt += "\n4) Use the writing style as per the following examples:\n"
+            for i, post in enumerate(examples):
+                post_text = post.get('text', '')
+                prompt += f'\nExample {i+1}: \n"{post_text}"\n'
+                if i == 1:
+                    break
+    except Exception:
+        pass
+
+    return prompt
